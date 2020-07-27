a05f4cdc500a341549e5eea237e5bb8eaa8a60b0
papuganivijaykumar <vijaykumar149909.com>
2020-07-27 15:43:48 +0530

    my fifth assignment

--- site/module4/easier/script.js
+++ site/module4/easier/script.js
@@ -53,11 +53,16 @@ for (var i = 0; i < names.length; i++) {
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  
+  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
   }
 }
+
 })();
